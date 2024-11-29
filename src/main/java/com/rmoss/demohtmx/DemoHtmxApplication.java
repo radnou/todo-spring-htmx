ab5a1587896bd518ebe4938b9a99136051495617
Radnoumane MOSSAELY <mossabelyradnoumane@gmail.com>
2024-11-29 03:20:53 +0100

    ✨ fonctionnalité(DemoHtmxApplication) : ajouter des méthodes pour créer et mettre à jour des tâches dans le contrôleur Todo 📝 documentation(application.properties) : activer le rechargement en direct et vérifier les modèles Thymeleaf ♻️ refactorisation(todos.html) : améliorer la structure et le style de la liste de tâches et du formulaire d'ajout

--- src/main/java/com/rmoss/demohtmx/DemoHtmxApplication.java
+++ src/main/java/com/rmoss/demohtmx/DemoHtmxApplication.java
@@ -1,5 +1,6 @@
 package com.rmoss.demohtmx;
 
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
@@ -7,8 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Comparator;
@@ -48,18 +52,41 @@ class TodoController {
 
     @DeleteMapping(produces = MediaType.TEXT_HTML_VALUE, path = "/{todoId}")
     @ResponseBody()
-    String delete(@PathVariable Integer todoId) {
+    String delete(@PathVariable Integer todoId,Model model) {
 
         this.todos
                 .stream()
                 .filter(t -> t.id().equals(todoId))
                 .forEach(this.todos::remove);
+        model.addAttribute("todos", this.todos);
 
         return "";
     }
 
+    @PostMapping
+    HtmxResponse create (@RequestParam("new-todo") String todo, Model model){
+        todos.add(Todos.todo(todo));
+        model.addAttribute("todos", this.todos);
+        return HtmxResponse.builder()
+                .view("todos :: todos")
+                .view("todos :: todos-form")
+                .build();
+    }
 
-
+    //patch method to update the status of a specify todo set in path
+    @PatchMapping(path = "/{todoId}")
+    @ResponseBody
+    String update(@PathVariable Integer todoId, @RequestParam(value = "todo-status", defaultValue = "false") Boolean completed, Model model) {
+        this.todos.stream()
+                .filter(t -> t.id().equals(todoId))
+                .findFirst()
+                .ifPresent(todo -> {
+                    this.todos.remove(todo);
+                    this.todos.add(new Todo(todo.id(), todo.title(), completed, todo.category()));
+                });
+        model.addAttribute("todos", this.todos);
+        return "";
+    }
 }
 
 record Todo(Integer id, String title, Boolean completed, String category) {
