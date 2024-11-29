15f735790171864e5db15ac48ebf0726ef1510c2
Radnoumane MOSSAELY <mossabelyradnoumane@gmail.com>
2024-11-29 02:15:24 +0100

    ♻️ refactor(DemoHtmxApplication.java) : supprimer les méthodes inutilisées et les importations non nécessaires pour alléger le code.

--- src/main/java/com/rmoss/demohtmx/DemoHtmxApplication.java
+++ src/main/java/com/rmoss/demohtmx/DemoHtmxApplication.java
@@ -8,11 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Comparator;
 import java.util.Set;
@@ -57,13 +54,6 @@ class TodoController {
                 .stream()
                 .filter(t -> t.id().equals(todoId))
                 .forEach(this.todos::remove);
-
-        return "";
-    }
-
-    @PostMapping()
-    @ResponseStatus()
-    String create (@RequestParam BodyP){
 
         return "";
     }
